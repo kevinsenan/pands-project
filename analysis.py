67ce502c62295e91c268d2f6fb224671dcff69c9
kevinsenan <kevinsenan@hotmail.com>
2023-05-04 16:02:16 +0100

    importing the iris data set

--- analysis.py
+++ analysis.py
@@ -4,8 +4,11 @@
 
 import pandas as pd
 
-# read in the data from the iris.data file as a csv file as that is how the data is presented and assign it to a datafile named df.
-df = pd.read_csv('iris.data')
+# read in the data from the iris.data file as a csv file 
+# as that is how the data is presented and assign it to a datafile 
+# named df. Header is set to None to ensure the first line 
+# of data isn't ignored as a header.
+df = pd.read_csv('iris.data', header=None)
 #test print to be removed
 print(df)
 
