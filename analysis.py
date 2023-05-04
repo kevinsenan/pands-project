2ea0acd730e291555ffeb1f189da36a2f1017204
kevinsenan <kevinsenan@hotmail.com>
2023-05-04 14:18:47 +0100

    reading the data in to from iris.data file

--- analysis.py
+++ analysis.py
@@ -1,3 +1,10 @@
 # analysis.py
 # Programming and Scripting Project. To analyse the Fisher's Iris data set using histogram and scatter plots
 # author K Donovan
+
+import pandas as pd
+
+# read in the data from the iris.data file as a csv file as that is how the data is presented and assign it to a datafile named df.
+df = pd.read_csv('iris.data')
+print(df)
+
